4efd125cd79f10486c25b9f4babe01abcc5577fe
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-22 17:03:51 +0000

    Deploy HummusHacks/hummushacks.github.io to github.com/HummusHacks/hummushacks.github.io.git:master

---
+++ pathfinder/setup.sh
@@ -0,0 +1,3 @@
+git clone https://${PERSONAL_GH_TOKEN}@github.com/${PERSONAL_GH_USER}/pathfinder-challenge.git --single-branch 
+cp pathfinder-challenge/frontend/* ${TRAVIS_BUILD_DIR}/pathfinder/
+rm -rf pathfinder-challenge
